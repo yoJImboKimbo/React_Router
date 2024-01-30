f320cbbb8c928f8503f27c6f3ffc1d77fa376a0a
bimo <jruiz6577@gmail.com>
2024-01-30 15:18:25 -0800

    displaying Nav using RouterProvider

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(<Route path="/" element={ <Root/> }></Route>))
 
 function App() {
   return (
-    // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={ appRouter } />
   );
 }
 
