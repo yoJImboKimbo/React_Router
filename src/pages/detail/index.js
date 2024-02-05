b7cf16982179e7fd8af90d45973978c4eee0fb06
bimo <jruiz6577@gmail.com>
2024-02-05 09:37:45 -0800

    added 404 pages for missing details with a root home button

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -34,6 +34,7 @@ const PetDetailsPage = () => {
       ) : error ? (
         <div>
           {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Navigate to='/pet-details-not-found'/>
         </div>
       ) : (
         <main>
